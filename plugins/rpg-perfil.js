e663a28ddd9aa1869385518e3be990e3252b351d
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-07-04 13:52:50 -0600

    Update rpg-perfil.js

--- plugins/rpg-perfil.js
+++ plugins/rpg-perfil.js
@@ -1,16 +1,21 @@
-import PhoneNumber from 'awesome-phonenumber';
-import fetch from 'node-fetch';
-
+import { createHash } from 'crypto'
+import PhoneNumber from 'awesome-phonenumber'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix }) => {
-    let user = global.db.data.users[m.sender];
-    let who = m.mentionedJid && m.mentionedJid.length > 0 ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-
-    
-    let { name, money, registered, age } = global.db.data.users[who] || {};
-    let username = conn.getName(who);
-    let prem = global.prems.includes(who.split`@`[0]);
-
-    let str = `
+let pp = './src/img/default_avatar.jpg'
+//const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
+let user = global.db.data.users[m.sender]
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+try {
+pp = await conn.getProfilePicture(who)         //pp = await conn.getProfilePicture(who)
+} catch (e) {
+} finally {
+let { name, money, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
+let mentionedJid = [who]
+let username = conn.getName(who)
+let prem = global.prems.includes(who.split`@`[0])
+let sn = createHash('md5').update(who).digest('hex')
+let str = `
 ╭━〔 👤 *PERFIL* 〕━⬣
 ┃ 𝙉𝙊𝙈𝘽𝙍𝙀 *:* ${username} ${user.registered ? '✓' : ''}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
@@ -20,22 +25,21 @@ let handler = async (m, { conn, usedPrefix }) => {
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ 𝙈𝙊𝙉𝙀𝘿𝘼𝙎 *:* ${money}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 𝘿𝙄𝘼𝙈𝘼𝙉𝙏𝙀𝙎 *:* ${limit}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ 𝙍𝙀𝙂𝙄𝙎𝙏𝙍𝘼𝘿𝙊(𝘼) *:* ${registered ? '✅' : '❎'}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃ 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 *:* ${prem ? '✅' : '❎'}
-╰━━━━〔 Airi 〕━━━⬣
-`.trim();
+╰━━━━〔 ${wm} 〕━━━⬣
+`.trim()
 
-    let buttons = [
-        { buttonId: '#minarcoins', buttonText: { displayText: 'Minar Monedas' }, type: 1 },
-        { buttonId: '#menu', buttonText: { displayText: 'Menú' }, type: 1 }
-    ];
 
-    await conn.sendButton(m.chat, str, '', null, buttons, m);
-};
+conn.sendFile(m.chat, pp, 'lp.jpg', str, m, false)
+}}
 
-handler.help = ['profile'];
-handler.tags = ['xp'];
-handler.command = /^perfil|profile?$/i;
 
-export default handler;
+handler.help = ['profile [@user]']
+handler.tags = ['xp']
+handler.command = /^perfil|profile?$/i
+export default handler
+    
