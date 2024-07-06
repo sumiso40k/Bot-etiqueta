7780221315359735f7299fe29afd64a91ce67752
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-07-06 05:35:17 -0600

    Create prueba.js

---
+++ plugins/prueba.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { generateWAMessageContent, generateWAMessageFromContent, proto } from '@whiskeysockets/baileys';
+
+let handler = async (message, { conn, text, usedPrefix, command }) => {
+    if (!text) {
+        return conn.sendMessage(message.key.remoteJid, { text: 'Escribe el texto que quieres buscar en Pinterest' });
+    }
+
+    async function getImageMessage(url) {
+        const { imageMessage } = await generateWAMessageContent({ image: { url: url } }, { upload: conn.waUploadToServer });
+        return imageMessage;
+    }
+
+    let response = await axios.get(`https://www.pinterest.com/search/pins/?q=${encodeURIComponent(text)}&rs=typed`);
+    let results = response.data.resource_response.data.results;
+    let imageUrls = results.map(result => result.images.orig.url);
+
+    let imageMessages = [];
+    for (let i = 0; i < imageUrls.length; i++) {
+        let url = imageUrls[i];
+        let imageMessage = await getImageMessage(url);
+        imageMessages.push({
+            imageMessage: imageMessage,
+            caption: `Resultado - ${i + 1}`
+        });
+    }
+
+    for (let msg of imageMessages) {
+        const messageContent = generateWAMessageFromContent(message.key.remoteJid, {
+            imageMessage: msg.imageMessage,
+            extendedTextMessage: { text: msg.caption }
+        });
+
+        await conn.relayMessage(message.key.remoteJid, messageContent.message, { messageId: messageContent.key.id });
+    }
+};
+
+handler.command = ['pinterest'];
+export default handler;
