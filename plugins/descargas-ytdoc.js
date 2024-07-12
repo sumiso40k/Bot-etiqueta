355d0c41b5889c994930f267fa2191b173d29ddd
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-07-11 18:01:26 -0600

    Create descargas-ytdoc.js

---
+++ plugins/descargas-ytdoc.js
@@ -0,0 +1,33 @@
+import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, args}) => {
+
+  if (!args[0]) throw m.reply('agrega un link');
+  await m.reply('Descargando');
+  try {
+    const qu = args[1] || '360';
+    const q = qu + 'p';
+    const v = args[0];
+    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+    const dl_url = await yt.video[q].download();
+    const ttl = await yt.title;
+    const size = await yt.video[q].fileSizeH;
+    const cap = `${ttl}\n${size}`.trim();
+    await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
+  } catch {
+    try {
+      const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`);
+      const lolh = await lolhuman.json();
+      const n = lolh.result.title || 'error';
+      const n2 = lolh.result.link;
+      const n3 = lolh.result.size;
+      const cap2 = `${n}\n ${n3}`.trim();
+      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m});
+    } catch {
+      await conn.reply(m.chat, 'no se pudo descargar', m);
+    }
+  }
+};
+handler.command = /^ytmp4doc|ytvdoc|ytmp4.2|ytv.2$/i;
+export default handler;
