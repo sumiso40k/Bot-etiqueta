89975a618b141f3b1f2f643b09c5faa4e60e138f
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-07-15 20:30:30 -0600

    Update descargas-tiktok.js

--- plugins/descargas-tiktok.js
+++ plugins/descargas-tiktok.js
@@ -15,38 +15,38 @@ let handler = async (m, { conn, text, args, usedPrefix, command}) => {
         const dataF = await tiktok.v1(args[0]);
         console.log(dataF);
         if (dataF && dataF.play) {
-            conn.sendFile(m.chat, dataF.play, 'tiktok.mp4', `_*☑️ Video de TikTok 1*_`, m);
+            conn.sendFile(m.chat, dataF.play, 'tiktok.mp4', `_*☑️ Video de TikTok*_`, m);
         } else {
             throw new Error('Video no encontrado');
         }
     } catch (e1) {
-        m.reply('reintentando 1/3');
+        
         try {
             const tTiktok = await tiktokdlF(args[0]);
             console.log(tTiktok);
             if (tTiktok && tTiktok.video) {
-                conn.sendFile(m.chat, tTiktok.video, 'tiktok.mp4', `_*☑️ Video de TikTok 2*_`, m);
+                conn.sendFile(m.chat, tTiktok.video, 'tiktok.mp4', `_*☑️ Video de TikTok*_`, m);
             } else {
                 throw new Error('Video no encontrado');
             }
         } catch (e2) {
-            m.reply('reintentando 2/3');
+            
             try {
                 let p = await fg.tiktok(args[0]);
                 console.log(p);
                 if (p && p.nowm) {
-                    conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `_*☑️ Video de TikTok 3*_`, m);
+                    conn.sendFile(m.chat, p.nowm, 'tiktok.mp4', `_*☑️ Video de TikTok*_`, m);
                 } else {
                     throw new Error('Video no encontrado');
                 }
             } catch (e3) {
-                m.reply('reintentando 3/3');
+            
                 try { 
                     const { author: { nickname }, video, description } = await tiktokdl(args[0]);
                     console.log(video);
                     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
                     if (url) {
-                        conn.sendFile(m.chat, url, 'tiktok.mp4', `_*☑️ Video de TikTok 4*_`, m);
+                        conn.sendFile(m.chat, url, 'tiktok.mp4', `_*☑️ Video de TikTok*_`, m);
                     } else {
                         throw new Error('Video no encontrado');
                     }
