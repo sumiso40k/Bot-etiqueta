2caf2145617f5940bf9510c5f5e641bb5a12f828
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-07-25 15:21:59 -0600

    Update descargas-ighistorias.js

--- plugins/descargas-ighistorias.js
+++ plugins/descargas-ighistorias.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat,       `${lenguajeGB['smsAvisoMG']()}𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙉𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙙𝙚 𝙄𝙂 𝙥𝙖𝙧𝙖 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝙡𝙖𝙨 𝙝𝙞𝙨𝙩𝙤𝙧𝙞𝙖\n𝙀𝙟:\n*${usedPrefix + command} gatadios*`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, body: wm, previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
+if (!args[0]) return conn.reply(m.chat,       `${lenguajeGB['smsAvisoMG']()}𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙚𝙡 𝙉𝙤𝙢𝙗𝙧𝙚 𝙙𝙚 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙙𝙚 𝙄𝙂 𝙥𝙖𝙧𝙖 𝙙𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝙡𝙖𝙨 𝙝𝙞𝙨𝙩𝙤𝙧𝙞𝙖\n𝙀𝙟:\n*${usedPrefix + command} usuario*`, m)
 handler.limit = false
 try {
-await conn.reply(m.chat,  `⌛ 𝐀𝐠𝐮𝐚𝐫𝐝𝐞 𝐮𝐧 𝐦𝐨𝐦𝐞𝐧𝐭𝐨 𝐲𝐚 𝐯𝐨𝐲 𝐜𝐨𝐧 𝐭𝐮 𝐩𝐞𝐫𝐝𝐢𝐝𝐨 🏃‍♂️...`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, body: ' 🚫𝐍𝐨 𝐇𝐚𝐠𝐚 𝐬𝐩𝐚𝐦 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})    
+await conn.reply(m.chat,  `⌛ Obteniendo resultados...`, m)  
 const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`)
 var anu = await res.json()
 var anuku = anu.result
@@ -22,5 +22,4 @@ handler.tags = ['downloader']
 handler.command = ['igstory', 'ighistoria', 'ighistorias' ]
 //handler.limit = 3
 //handler.exp = 87
-handler.register = true
 export default handler
