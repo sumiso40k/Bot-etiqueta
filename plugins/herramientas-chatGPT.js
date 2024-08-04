489ff6169d0f44b1126a00a4036a276b0ac6725f
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-04 14:30:26 -0600

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -4,18 +4,27 @@ import axios from 'axios';
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 
-if (!text) return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_.${command} Recomienda un top 10 de películas de acción_`, m)
-
-try {
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
-let res = await gpt.json()
-await m.reply(res.data)
-} catch {
-try {
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
-let res = await gpt.json()
-await m.reply(res.gpt)
-} catch {
-}}};
+    if (!text) {
+        return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_.${command} Recomienda un top 10 de películas de acción_`, m)
+    }
+    
+    try {
+        //let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
+        
+        let ia = await fetch(`https://api.diego-ofc.store/ai/bing?username=airi&query=${text}`);
+        let res = await ia.json();
+        await m.reply(res.msg);
+    } catch {
+        /*
+        try {
+            let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
+            let res = await gpt.json()
+            await m.reply(res.gpt)
+        } catch {
+            
+        }
+        */
+    }
+};
 handler.command = ['ia']
 export default handler;
