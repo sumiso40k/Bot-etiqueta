eb261bb54630a91b20200d408103f7a79ac81a86
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-04 14:35:09 -0600

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -1,8 +1,13 @@
-import chalk from 'chalk';
+import {Chalk} from 'chalk';
 
+const customChalk = new Chalk({level: 3});
 let handler = async (m, { conn }) => {
         m.reply(`Hola mundo`);
-        console.log(chalk.blue('╭━❬ ✤ Airi-Bot ✤ ❭━╮\n│ARCHIVOS DE LA CARPETA TMP ELIMINADOS\n╰━➤'));
+        
+        console.log(customChalk.hex('#E991E6').bold('╭━❬ ✤ Airi-Bot ✤ ❭━╮\n│ARCHIVOS DE LA CARPETA TMP ELIMINADOS\n╰━➤'));
+        
+        //console.log(chalk.blue('╭━❬ ✤ Airi-Bot ✤ ❭━╮\n│ARCHIVOS DE LA CARPETA TMP ELIMINADOS\n╰━➤'));
+
 }
 
 handler.command = ['hola']
