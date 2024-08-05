d1ad5f8be30132207a8c20a7aefc80a6f74f3167
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-05 10:14:52 -0600

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,19 +1,30 @@
 import fetch from 'node-fetch';
+import axios from 'axios';
 
-const handler = async (m, {conn, text}) => {
+
+const handler = async (m, {conn, text, usedPrefix, command}) => {
 
     if (!text) {
-        return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_Envía un mensaje para obtener una respuesta de ChatGPT_`, m);
+        return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_.${command} Recomienda un top 10 de películas de acción_`, m)
     }
     
     try {
-        let ia = await fetch(`https://api.diego-ofc.store/ai/bing?username=airi&query=${text}`);
+        //let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
+        
+        let ia = await fetch(`https://api.diego-ofc.store/ai/gpt4?username=airi&query=${text}`);
         let res = await ia.json();
         await m.reply(res.msg);
     } catch {
-        // Manejo de errores en caso de que la API falle.
-        await m.reply(`_*[ ⚠️ ] Hubo un error al procesar tu solicitud. Inténtalo de nuevo más tarde.*_`, m);
+        /*
+        try {
+            let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
+            let res = await gpt.json()
+            await m.reply(res.gpt)
+        } catch {
+            
+        }
+        */
     }
 };
-
+handler.command = ['ia']
 export default handler;
