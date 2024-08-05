a778778e0d26800d4f94f73b545e6e40d191cd61
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-05 10:08:09 -0600

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -1,4 +1,27 @@
 import fetch from 'node-fetch';
+
+const handler = async (m, {conn, text}) => {
+
+    if (!text) {
+        return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_Envía un mensaje para obtener una respuesta de ChatGPT_`, m);
+    }
+    
+    try {
+        let ia = await fetch(`https://api.diego-ofc.store/ai/bing?username=airi&query=${text}`);
+        let res = await ia.json();
+        await m.reply(res.msg);
+    } catch {
+        // Manejo de errores en caso de que la API falle.
+        await m.reply(`_*[ ⚠️ ] Hubo un error al procesar tu solicitud. Inténtalo de nuevo más tarde.*_`, m);
+    }
+};
+
+export default handler;
+
+
+
+/*
+import fetch from 'node-fetch';
 import axios from 'axios';
 
 
@@ -28,3 +51,4 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
 };
 handler.command = ['ia']
 export default handler;
+*/
