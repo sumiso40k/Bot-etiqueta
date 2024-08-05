4ee07ec264d26197b9086735590b47c0d9918191
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-05 10:08:53 -0600

    Update herramientas-chatGPT.js

--- plugins/herramientas-chatGPT.js
+++ plugins/herramientas-chatGPT.js
@@ -17,38 +17,3 @@ const handler = async (m, {conn, text}) => {
 };
 
 export default handler;
-
-
-
-/*
-import fetch from 'node-fetch';
-import axios from 'axios';
-
-
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-
-    if (!text) {
-        return conn.reply(m.chat, `_*[ ⚠️ ] Escribe lo que quieras a ChatGPT*_\n\n> Ejemplo:\n_.${command} Recomienda un top 10 de películas de acción_`, m)
-    }
-    
-    try {
-        //let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
-        
-        let ia = await fetch(`https://api.diego-ofc.store/ai/gpt4?username=airi&query=${text}`);
-        let res = await ia.json();
-        await m.reply(res.msg);
-    } catch {
-        /*
-        try {
-            let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
-            let res = await gpt.json()
-            await m.reply(res.gpt)
-        } catch {
-            
-        }
-        */
-    }
-};
-handler.command = ['ia']
-export default handler;
-*/
