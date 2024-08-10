fc913aac8f277dfd4f124f5bc81ea6c0d41d2431
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-10 11:29:48 -0600

    Update Prueba6yt.js

--- plugins/Prueba6yt.js
+++ plugins/Prueba6yt.js
@@ -2,18 +2,18 @@ import fetch from 'node-fetch';
 
 const handler = async (m, { conn, args, command, usedPrefix }) => {
     if (!args[0]) {
-        return conn.reply(m.chat, `_*[ ⚠️ ] Agrega un enlace de Facebook *_\n\n> Ejemplo:\n_.fbvideo https://www.facebook.com/reel/443838421898212?mibextid=x2TFxBDZxubyk4Hj_`, m);
+        return conn.reply(m.chat, `_*[ ⚠️ ] Agrega un enlace de Youtube*_`, m);
     }
 
     try {
         await conn.reply(m.chat, `_*[ ⏳ ] Descargando el video...*_`, m);
 
-        const apiUrl = `https://api-airi.vercel.app/ytvmp4?url=${encodeURIComponent(args[0])}`;
+        const apiUrl = `https://anydl.guruapi.tech/ytdl/v2/ytmp4?url=${encodeURIComponent(args[0])}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
-        if (data.result && data.result.downloadUrl) {
-            const downloadUrl = data.result.hd || data.result.sd || data.result.downloadUrl;
+        if (data.result && data.result.dlink) {
+            const downloadUrl = data.result.hd || data.result.sd || data.result.dlink;
             const filename = `${data.result.title || 'video'}.mp4`;
             const thumb = data.result.thumbnail
             await conn.sendMessage(m.chat, { video: { url: downloadUrl }, fileName: `${filename}.mp4`, mimetype: 'video/mp4', caption: `╭━❰  *YOUTUBE*  ❱━⬣\n${filename}\n╰━❰ *${wm}* ❱━⬣`, thumbnail: await fetch(thumb) }, { quoted: m })
