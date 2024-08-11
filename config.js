8336cd05582795faa1dfddfd0c2938a9fc3d225a
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-11 16:02:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -68,7 +68,7 @@ global.APIKeys = {'https://api.xteam.xyz': `${keysxteam}`,
 'https://api-xcoders.site': 'Frieren' }
 
 global.mods = [] 
-global.cheerio = load
+global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
