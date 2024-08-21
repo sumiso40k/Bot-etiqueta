8f7d9eb821e9cef738ebabe785fd09e20ac0f178
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-20 19:50:43 -0600

    Update descargas-yta.js

--- plugins/descargas-yta.js
+++ plugins/descargas-yta.js
@@ -1,6 +1,6 @@
 
 
-/*import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
 import fetch from 'node-fetch'
 import yts from 'yt-search'
 import ytdl from 'ytdl-core'
@@ -49,4 +49,4 @@ try {
 
 handler.command = ['ytmp3', 'yta']
 export default handler
-*/
+
