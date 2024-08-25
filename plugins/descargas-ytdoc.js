787bfc1f3ba4f9eaf06b3aa3aef8ab8271afc480
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-25 10:24:16 -0600

    Update descargas-ytdoc.js

--- plugins/descargas-ytdoc.js
+++ plugins/descargas-ytdoc.js
@@ -1,4 +1,4 @@
-
+/*
 import fetch from 'node-fetch';
 
 const getYoutubeId = (url) => {
@@ -100,3 +100,42 @@ if (command==='ytmp3doc'){
 handler.command = ['ytmp4doc', 'ytmp3doc'];
 export default handler;
       
+*/
+
+
+
+import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, args}) => {
+
+  if (!args[0]) throw m.reply('agrega un link');
+  await m.reply('Descargando');
+  try {
+    const qu = args[1] || '360';
+    const q = qu + 'p';
+    const v = args[0];
+    const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+    const dl_url = await yt.video[q].download();
+    const ttl = await yt.title;
+    const size = await yt.video[q].fileSizeH;
+    const cap = `${ttl}`.trim();
+    await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
+    //await await conn.sendMessage(m.chat, {video: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
+  } catch {
+    try {
+      const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`);
+      const lolh = await lolhuman.json();
+      const n = lolh.result.title || 'error';
+      const n2 = lolh.result.link;
+      const n3 = lolh.result.size;
+      const cap2 = `${n}`.trim();
+      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m});
+    } catch {
+      await conn.reply(m.chat, 'No se pudo descargar', m);
+    }
+  }
+};
+handler.command = ['ytvdoc', 'ytmp4doc'];
+export default handler;
+                               
