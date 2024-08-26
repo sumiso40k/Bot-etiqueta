cd14aa9b12133d6ee7ec69f41d0c72409e7ef716
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-26 06:22:44 -0600

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,3 +1,61 @@
+
+
+import axios from 'axios';
+import fetch from 'node-fetch';
+import { parse } from 'node-html-parser';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `_*[ ⚠️ ] Agrega un enlace de mediafire*_`;
+  try {
+    let res = await mediafireDl(args[0]);
+    let { name, size, date, mime, link } = res;
+    let caption = `
+> ┃ 𝗠𝗘𝗗𝗜𝗔𝗙𝗜𝗥𝗘  
+> ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+> ┃ 💫 *Nombre*
+> ┃ ${name}
+> ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+> ┃ 💪 *Tamaño*
+> ┃ ${size}
+> ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+> ┃ 🚀 *Tipo*
+> ┃ ${mime}`.trim();
+    conn.reply(m.chat, caption, m);
+    await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true });
+  } catch (e) {
+    await conn.reply(m.chat, `_*[ ❌ ] Ocurrió un error al descargar el archivo*_`, m);
+    console.log(e);
+  }
+  handler.limit = false;
+};
+
+handler.help = ['mediafire'].map(v => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i;
+
+export default handler;
+
+async function mediafireDl(url) {
+  const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/', '')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`);
+  const root = parse(res.data);
+
+  const link = root.querySelector('#downloadButton').getAttribute('href');
+  const name = root.querySelector('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').getAttribute('title').replaceAll(' ', '').replaceAll('\n', '');
+  const date = root.querySelector('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text;
+  const size = root.querySelector('#downloadButton').text.replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('                         ', '').replaceAll(' ', '');
+
+  let mime = '';
+  let rese = await axios.head(link);
+  mime = rese.headers['content-type'];
+
+  return { name, size, date, mime, link };
+}
+
+
+
+
+
+/*
 import axios from 'axios';
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
@@ -42,3 +100,4 @@ async function mediafireDl(url) {
   mime = rese.headers['content-type'];
   return {name, size, date, mime, link};
 }
+*/
