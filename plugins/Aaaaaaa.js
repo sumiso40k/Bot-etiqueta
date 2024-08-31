1768dbaaef0c791c9fb5fedbc6675d7207b6c1dc
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-08-31 14:14:06 -0600

    Create Aaaaaaa.js

---
+++ plugins/Aaaaaaa.js
@@ -0,0 +1,18 @@
+import { alldl } from 'rahad-all-downloader';
+
+let handler = async (m, { conn, text }) => {
+    const videoUrl = text.trim(); // Extrae la URL del mensaje
+    if (!videoUrl) {
+        return m.reply('Por favor, proporciona la URL del video.');
+    }
+    try {
+        const result = await alldl(videoUrl);
+        const message = `Resultado:\n${JSON.stringify(result, null, 2)}`;
+        m.reply(message);
+    } catch (error) {
+        m.reply(`Error: ${error.message}`);
+    }
+};
+
+handler.command = /^(descargar1|download1)$/i;
+export default handler;
