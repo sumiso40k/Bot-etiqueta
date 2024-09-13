fc80fb355a78e61742f0dbea7e93644ac51509db
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-09-12 19:38:34 -0600

    Update tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -10,7 +10,7 @@ let userid = conn.getName(m.sender) || 'default';
 let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
 let result = await fetch(apiurl);
 let response = await result.json();
-  let mesg = result.msg;
+  let mesg = response.msg;
 
 await conn.reply(m.chat, mesg);
 } catch {}}
