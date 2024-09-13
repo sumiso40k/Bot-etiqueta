1a9c88deb484eff9215ec49d914f530fd31b7876
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-09-12 19:36:13 -0600

    Update tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -4,14 +4,15 @@ const handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, 'Escribe un texto para hablar con chatgpt', m);
 
 try {
-let msg = await conn.sendMessage(m.chat, {text: '*chatgpt esta escribiendo.....*'});
+//let msg = await conn.sendMessage(m.chat, {text: '*chatgpt esta escribiendo.....*'});
 
 let userid = conn.getName(m.sender) || 'default';
 let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
 let result = await fetch(apiurl);
 let response = await result.json();
+  let mesg = result.msg;
 
-await conn.relayMessage(m.chat, { protocolMessage: { key: msg.key, type: 14, editedMessage: { conversation: response.msg }}}, {});
+await conn.reply(m.chat, mesg);
 } catch {}}
 
 handler.command = handler.help = ["chatgpt"];
