fcb5353fb90ca454754f6588d501314b921a8fc0
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-04 16:51:54 -0600

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,18 +1,21 @@
+
 import fetch from 'node-fetch';
 
 let handler = m => m
 
 handler.all = async function (m, {conn, text}) {
-let chat = global.db.data.chats[m.chat]
-    
-if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
+    let chat = global.db.data.chats[m.chat]
 
-let gpt = await fetch(`https://api.dorratz.com/ai/gpt4?username=user&query=${text}`)
-let res = await gpt.json()
-await m.reply(res.msg)
+    if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 
+        // Eliminar la mención del texto
+        let cleanText = text.replace(/@\d+/g, '').trim();
+
+        let gpt = await fetch(`https://api.dorratz.com/ai/gpt4?username=user&query=${encodeURIComponent(cleanText)}`)
+        let res = await gpt.json()
+        await m.reply(res.msg)
+    }
+
+    return !0 
 }
-
-
-return !0 }
 export default handler
