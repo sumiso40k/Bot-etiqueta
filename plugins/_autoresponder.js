c85b7360a642d4085e34c2d9ad6b13bc9141fd51
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-04 17:00:01 -0600

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,4 +1,3 @@
-
 import fetch from 'node-fetch';
 
 let handler = m => m
@@ -8,14 +7,20 @@ handler.all = async function (m, {conn, text}) {
 
     if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 
-        // Eliminar la mención del texto
-        let cleanText = text.replace(/@\d+/g, '').trim();
+        // Verificar si 'text' existe y es una cadena
+        if (typeof text === 'string') {
+            // Eliminar la mención del texto
+            let cleanText = text.replace(/@\d+/g, '').trim();
 
-        let gpt = await fetch(`https://api.dorratz.com/ai/gpt4?username=user&query=${encodeURIComponent(cleanText)}`)
-        let res = await gpt.json()
-        await m.reply(res.msg)
+            let gpt = await fetch(`https://api.dorratz.com/ai/gpt4?username=user&query=${encodeURIComponent(cleanText)}`)
+            let res = await gpt.json()
+            await m.reply(res.msg)
+        } else {
+            await m.reply('No se ha detectado texto después de la mención.')
+        }
     }
 
     return !0 
 }
 export default handler
+                          
