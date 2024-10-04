4e78670eee094b3e539fd076e42857fa6d320ab4
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-04 17:03:21 -0600

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -2,16 +2,18 @@ import fetch from 'node-fetch';
 
 let handler = m => m
 
-handler.all = async function (m, {conn, text}) {
+handler.all = async function (m, {conn}) {
     let chat = global.db.data.chats[m.chat]
 
     if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 
-        // Verificar si 'text' existe y es una cadena
-        if (typeof text === 'string') {
-            // Eliminar la mención del texto
-            let cleanText = text.replace(/@\d+/g, '').trim();
+        // Obtener el texto directamente de m.text
+        let text = m.text || '';
 
+        // Eliminar la mención del texto
+        let cleanText = text.replace(/@\d+/g, '').trim();
+
+        if (cleanText) {
             let gpt = await fetch(`https://api.dorratz.com/ai/gpt4?username=user&query=${encodeURIComponent(cleanText)}`)
             let res = await gpt.json()
             await m.reply(res.msg)
@@ -23,4 +25,3 @@ handler.all = async function (m, {conn, text}) {
     return !0 
 }
 export default handler
-                          
