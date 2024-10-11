33390fe1a76ec98c83a7cfd25ac5be3c2f594f10
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-10 19:23:10 -0600

    Update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -1,11 +1,11 @@
 import { sticker } from '../lib/sticker.js'
-import { botnam } from '../exports.js'
+import { botnam, icon1 } from '../exports.js'
 
 let MessageType = (await import(global.baileys)).default
 import fetch from 'node-fetch'
 import fs from "fs"
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, `⚠️ 𝘿𝙚𝙗𝙚𝙨 𝙙𝙚 𝙪𝙨𝙖𝙧 2 𝙚𝙢𝙤𝙟𝙞𝙨 𝙮 𝙚𝙣 𝙢𝙚𝙙𝙞𝙤 𝙪𝙨𝙖𝙧 𝙚𝙡 *+*\n• 𝙀𝙟𝙚𝙢𝙥𝙡𝙤 :\n*${usedPrefix + command}* 😺+😆`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: botnam, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
+if (!args[0]) return conn.reply(m.chat, `⚠️ 𝘿𝙚𝙗𝙚𝙨 𝙙𝙚 𝙪𝙨𝙖𝙧 2 𝙚𝙢𝙤𝙟𝙞𝙨 𝙮 𝙚𝙣 𝙢𝙚𝙙𝙞𝙤 𝙪𝙨𝙖𝙧 𝙚𝙡 *+*\n• 𝙀𝙟𝙚𝙢𝙥𝙡𝙤 :\n*${usedPrefix + command}* 😺+😆`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: botnam, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: icon1, sourceUrl: redes.getRandom()}}})
 try {
 let [emoji1, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
