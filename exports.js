52d1e34b2856af9d209d565f8386aceda84f6d78
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-10 19:29:09 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -1,2 +1,11 @@
 export const botnam = '✿ 𝐀𝐢𝐫𝐢 ✿';
+
 export const icon1 = fs.readFileSync('./src/img/icon1.jpg');
+export const icon2 = fs.readFileSync('./src/img/icon2.jpg');
+export const icon3 = fs.readFileSync('./src/img/icon3.jpg');
+export const icon4 = fs.readFileSync('./src/img/icon4.jpg');
+export const icon5 = fs.readFileSync('./src/img/icon5.jpg');
+
+export const icons = fs.readFileSync('./src/img/icon1.jpg')
+
+
