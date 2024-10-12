53759693f0f06c600d2214de13b2264529891d09
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-12 07:25:23 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -8,6 +8,6 @@ export const icon5 = fs.readFileSync('./src/img/icon5.jpg');
 
 export const icons = [icon1, icon2, icon3, icon4, icon5];
 
-export const contextInfo1 = {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: iig, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: icons.getRandom(), sourceUrl: redes.getRandom()}}}
+export const contextInfo1 = contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: iig, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: icons.getRandom(), sourceUrl: redes.getRandom()}}
 
 
