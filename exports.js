8faa7c9dc85954262d52b46411e37db4c61a85fe
Rudy Vasquez <89273857+Rudyrex@users.noreply.github.com>
2024-10-12 07:15:17 -0600

    Update exports.js

--- exports.js
+++ exports.js
@@ -8,6 +8,6 @@ export const icon5 = fs.readFileSync('./src/img/icon5.jpg');
 
 export const icons = [icon1, icon2, icon3, icon4, icon5];
 
-export const contextInfo1 = `{contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: 'desc', title: '✿ 𝐀𝐢𝐫𝐢 ✿', body: '𝙴𝚕 𝚖𝚎𝚓𝚘𝚛 𝚋𝚘𝚝 𝚍𝚎 𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 ✅', previewType: 0, thumbnail: icons.getRandom(), sourceUrl: 'https://github.com/Rudyrex/Airi-Bot'}}}`;
+export const contextInfo1 = {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: iig, body: ' 💫 𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 🥳 ', previewType: 0, thumbnail: icons.getRandom(), sourceUrl: redes.getRandom()}}}
 
 
